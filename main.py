4bed5c81cb4570b7fb8cbb9f4b6f1405e7f8425e
= <daine.bigham@gmail.com>
2021-06-30 21:06:13 +1200

    created clear screen function and put the rest of the code inside a main function

--- main.py
+++ main.py
@@ -6,38 +6,46 @@ USER_AGENT = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTM
 
 headers = {'user-agent': USER_AGENT}
 
-while True:
-    query = input('Ask Jeeves: ')
-    query = query.replace(' ', '+')
-    url = f'https://google.com/search?q={query}'
+def main():
+    while True:
+        clear()
+        query = input('Ask Jeeves: ')
+        query = query.replace(' ', '+')
+        url = f'https://google.com/search?q={query}'
 
-    response = requests.get(url, headers=headers)
+        response = requests.get(url, headers=headers)
 
-    if response.status_code == 200: 
-        soup = BeautifulSoup(response.text, 'html.parser')
+        if response.status_code == 200: 
+            soup = BeautifulSoup(response.text, 'html.parser')
 
-    answer_one = soup.find('span', {'class': 'hgKElc'})
-    try:
-        print(answer_one.text)
-    except AttributeError:
-        answer_two = soup.find('div', class_='Z0LcW')
+        answer_one = soup.find('span', {'class': 'hgKElc'})
         try:
-            print(answer_two.text)
+            print(answer_one.text)
         except AttributeError:
-            answer_three = soup.find_all('li', class_='TrT0Xe')
-            if len(answer_three) > 0:
-                try:
-                    for answer in answer_three[:10]:   
-                        print(answer.text.split('.')[0])
-                except AttributeError:
-                    print('Unable to find an answer. Sorry :(')
-            else: 
-                answer_four = soup.find_all('a', class_='ct5Ked')
-                try:
-                    for answer in answer_four[:10]:
-                        print(answer['data-entityname'])
-                except AttributeError:
-                    print('Unable to find an answer. Sorry :(')
+            answer_two = soup.find('div', class_='Z0LcW')
+            try:
+                print(answer_two.text)
+            except AttributeError:
+                answer_three = soup.find_all('li', class_='TrT0Xe')
+                if len(answer_three) > 0:
+                    try:
+                        for answer in answer_three[:10]:   
+                            print(answer.text.split('.')[0])
+                    except AttributeError:
+                        print('Unable to find an answer. Sorry :(')
+                else: 
+                    answer_four = soup.find_all('a', class_='ct5Ked')
+                    try:
+                        for answer in answer_four[:10]:
+                            print(answer['data-entityname'])
+                    except AttributeError:
+                        print('Unable to find an answer. Sorry :(')
+
+def clear(): 
+    os.system('cls' if os.name == 'nt' else 'clear')
+    input()
+
+main()
 
 
 
